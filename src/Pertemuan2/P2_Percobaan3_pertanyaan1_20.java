d613bada9beffd4820e33c0e14bc01d533819d2e
zuhdi <zuhdiyuda21@gmail.com>
2024-09-12 07:48:02 +0700

    data krs 1

---
+++ src/Pertemuan2/P2_Percobaan3_pertanyaan1_20.java
@@ -0,0 +1,22 @@
+package Pertemuan2;
+public class P2_Percobaan3_pertanyaan1_20 {
+
+    public static void main(String[] args) {
+        System.out.println("                           KARTU RENCANA STUDI (KRS)");;
+        System.out.println("NAMA                : Muhammad Zuhdi Yudadharma");
+        System.out.println("PROGRAM STUDI       : D-IV Teknik Informatika");
+        System.out.println("KELAS               : 1G");
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.println(" KODE MK                MATA KULIAH                  SEMESTER       SKS | JAM ");
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("|RT1241001 |  Pancasila                              |  1  |        |2  |  2|");
+        System.out.println("|RT1241002 |  Konsep Teknologi Informasi             |  1  |        |2  |  4|");
+        System.out.println("|RT1241003 |  Critical Thinking dan Problem Solving  |  1  |        |2  |  4|");
+        System.out.println("|RT1241004 |  Matematika Dasar                       |  1  |        |3  |  6|");
+        System.out.println("|RT1241005 |  Rekayasa Perangkat Lunak               |  1  |        |2  |  4|");
+        System.out.println("|RT1241006 |  Dasar Pemrograman                      |  1  |        |2  |  4|");
+        System.out.println("|RT1241007 |  Praktikum Dasar Pemrograman            |  1  |        |3  |  6|");
+        System.out.println("|RT1241008 |  Keselamatan dan Kesehatan Kerja        |  1  |        |2  |  4|");
+        System.out.println("------------------------------------------------------------------------------");
+    }
+}
