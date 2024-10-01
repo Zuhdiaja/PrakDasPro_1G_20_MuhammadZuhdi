aa84620337f15c23dd5c19e19c3b897805d6833c
zuhdi <zuhdiyuda21@gmail.com>
2024-10-01 20:48:14 +0700

    P5_Percobaan3_PertanyaanNo4

--- src/pertemuan5/PemilihanHariDenganIf20.java
+++ src/pertemuan5/PemilihanHariDenganIf20.java
@@ -9,7 +9,7 @@ public class PemilihanHariDenganIf20 {
     int angka = sc.nextInt();
     
         if (angka >= 1 && angka <= 5) {
-            System.out.println("Weekday");
+            System.out.println("Weekdayy");
         } else if (angka == 6 || angka == 7) {
             System.out.println("Weekend");
         } else {
