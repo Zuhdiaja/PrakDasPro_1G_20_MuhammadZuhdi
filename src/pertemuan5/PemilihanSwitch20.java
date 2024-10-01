9dc507168b64077609b6c1e7ea5139be51ef2101
zuhdi <zuhdiyuda21@gmail.com>
2024-10-01 12:58:14 +0700

    P5_Percobaan2_PertanyaanNo3

--- src/pertemuan5/PemilihanSwitch20.java
+++ src/pertemuan5/PemilihanSwitch20.java
@@ -29,6 +29,9 @@ public class PemilihanSwitch20 {
             case '/':
                 hasil = angka1 / angka2;
                 break;
+            default:
+                System.out.println("Operator yang dipilih tidak sesuai.");
+                break;
         }
         System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
     }
