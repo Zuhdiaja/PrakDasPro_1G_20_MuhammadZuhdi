599dbca8e3db6802060c3bad8edd8550d48afe05
zuhdi <zuhdiyuda21@gmail.com>
2024-10-01 11:39:45 +0700

    P5_Percobaan1_PertanyaanNo1

--- src/pertemuan5/PemilihanBilangan20.java
+++ src/pertemuan5/PemilihanBilangan20.java
@@ -3,20 +3,12 @@ import java.util.Scanner;
 
 public class PemilihanBilangan20 {
     public static void main(String[] args) {
-        int angka;
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("masukkan sebuah angka:");
-        angka = sc.nextInt();
-        
-        if (angka % 2 == 0)
-        {
-            System.out.println("angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("angka " + angka + " termasuk bilangan ganjil");
-        }
-    }
+        int angka = sc.nextInt();
+        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+    
+    System.out.println("angka " + angka + " termasuk bilangan " + hasil + ".");
+}
 }
