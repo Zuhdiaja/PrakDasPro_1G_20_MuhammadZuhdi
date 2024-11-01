88a691fe2265ece78b6fa37821e16c4d601206fe
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2024-11-01 07:47:30 +0700

    Pertanyaan5_percobaan2

--- src/Pertemuan10/ArrayNilai20.java
+++ src/Pertemuan10/ArrayNilai20.java
@@ -7,13 +7,18 @@ public class ArrayNilai20 {
 
         int [] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i <nilaiAkhir.length; i++) {
             System.out.print("masukkan nilai akhir ke-" +i+ " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("nilai akhir ke-" +i+ " adalah " +nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("mahasiswa ke-" +i+ " lulus " );
+            } else if (nilaiAkhir[i] <= 69) {
+                System.out.println("mahasiswa ke-" +i+ " tidak lulus ");
+            }
+        
         }
     }
 }
