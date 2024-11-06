2067f074baf0fd00796e92358d5ed190d5542735
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2024-11-06 15:31:02 +0700

    Pertanyaan4_Percobaan4

--- src/Pertemuan10/SearchNilai20.java
+++ src/Pertemuan10/SearchNilai20.java
@@ -4,18 +4,37 @@ import java.util.Scanner;
 public class SearchNilai20 {
     
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < arrNilai.length; i++) {
+        int key;
+        int hasil = -1;
+
+        System.out.print("masukkan banyak nilai yang akan diinput  : ");
+        int jmlNilai = sc.nextInt();
+        int[] arrNilai = new int[jmlNilai];
+
+        for (int i = 0; i < jmlNilai; i++) {
+            System.out.print("masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("masukkan nilai yang ingin dicari : ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < jmlNilai; i++) {
             if (key == arrNilai[i]) {
                 hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("nilai " +key+ " ketemu di indeks ke-" +hasil);
-        System.out.println();
+
+        if (hasil != -1) {
+            System.out.println("nilai " + key + "  ketemu di mahasiswa " + (hasil+1));
+        }else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
+        
+
+
     }
 }
