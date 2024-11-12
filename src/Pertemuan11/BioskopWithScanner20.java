19c7a0dc5b3c94192a50157cc7bd52fda4402d53
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2024-11-12 15:46:07 +0700

    percobaan2_pertanyaan6

--- src/Pertemuan11/BioskopWithScanner20.java
+++ src/Pertemuan11/BioskopWithScanner20.java
@@ -11,6 +11,8 @@ public class BioskopWithScanner20 {
         String [][] penonton = new String[4][2];
     
         while (true) {
+            System.out.println();
+            System.out.println("input");
             System.out.print("masukkan nama: ");
             nama = sc.nextLine();
             System.out.print("masukkan baris: ");
@@ -19,7 +21,26 @@ public class BioskopWithScanner20 {
             kolom = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris][kolom-1] = nama;
+            if (baris >= 0 && baris <= penonton.length && kolom >= 0 && kolom <= penonton[0].length) {
+                if (penonton [baris-1][kolom-1] == null) {
+                    penonton [baris-1][kolom-1] = nama;
+                } else {
+                    System.out.println("masukkan baris dan kolom kembali");
+                }
+            } else {
+                System.out.println("baris/kolom kursi yang tidak tersedia");
+                
+            }
+            System.out.println();
+            System.out.println("tampilan");
+            
+            for (int i = 0; i < penonton.length; i++) {
+                System.out.print("penonton pada baris ke-" + (i+1) + ": ");
+                for (int j = 0; j < penonton[i].length; j++) {
+                    System.out.print((penonton[i][j] == null ? "***" : penonton[i][j]) + " ");
+                }
+                System.out.println();
+            }
 
             System.out.print("input penonton lainnya? (y/n): ");
             next = sc.nextLine();
