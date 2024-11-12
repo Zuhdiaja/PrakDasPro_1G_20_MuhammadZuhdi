f1f17fe591e0bd13e4c764d4cbc04ca8e70aa48f
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2024-11-12 16:23:56 +0700

    percobaan3_pertanyaan5

--- src/Pertemuan11/Numbers20.java
+++ src/Pertemuan11/Numbers20.java
@@ -1,5 +1,7 @@
 package Pertemuan11;
 
+import java.util.Arrays;
+
 public class Numbers20 {
     public static void main(String[] args) {
         
@@ -7,5 +9,13 @@ public class Numbers20 {
         myNumbers [0] = new int[5];
         myNumbers [1] = new int[3];
         myNumbers [2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
 }
