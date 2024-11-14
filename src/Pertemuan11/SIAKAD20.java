bf724ade6c33eec89634f5e70c09b83a80b43a60
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2024-11-14 14:09:11 +0700

    percobaan4_pertanyaan2

--- src/Pertemuan11/SIAKAD20.java
+++ src/Pertemuan11/SIAKAD20.java
@@ -5,30 +5,37 @@ public class SIAKAD20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [][] nilai = new int[4][3];
+        int mahasiswa,matkul;
+
+        System.out.print("Masukan jumlah mahasiswa: ");
+        mahasiswa = sc.nextInt();
+        System.out.print("Masukan jumlah mata kuliah: ");
+        matkul = sc.nextInt();
+
+        int [][] nilai = new int[mahasiswa][matkul];
 
         for (int i = 0; i < nilai.length; i++) {
-            System.out.println("input nilai mahasiswa ke-" + (i+1));
+            System.out.println("Input nilai mahsiswa ke-" + (i+1));
             double totalPerSiswa = 0;
-
-            for (int j = 0; j < nilai[i].length; j++) {
+            for (int j = 0; j < nilai[0].length; j++) {
                 System.out.print("Nilai mata kuliah " + (j+1) + ": ");
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
-            System.out.println("nilai rata-rata: " + totalPerSiswa/3);
+
+            System.out.println("Nilai rata-rata mahasiswa: " + totalPerSiswa/matkul);
         }
-        
-        System.out.println("=======================");
-        System.out.println("rata rata nilai mata kuliah");
+        System.out.println("=============================");
+        System.out.println("Rata-rata Nilai Mata Kuliah: ");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < matkul; j++) {
             double totalPerMatkul = 0;
-
-            for (int i = 0; i < 4; i++) {
+            
+            for (int i = 0; i < mahasiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("mata kuliah " + (j+1) + ": " + totalPerMatkul/4);
+            System.out.println("mata Kuliah " + (j+1) + ": " + totalPerMatkul/mahasiswa);
         }
+        
     }
 }
