5c504d9981402c4a6f4f2e9e535a38d96e096693
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2024-11-29 13:58:02 +0700

    P13_percobaan2_pertanyaan5

--- src/Pertemuan13/Kafe20.java
+++ src/Pertemuan13/Kafe20.java
@@ -2,12 +2,13 @@ package Pertemuan13;
 
 public class Kafe20 {
 
-    public static void menu(String namaPelanggan, boolean isMember) {
+    public static void menu(String namaPelanggan, boolean isMember, String KodePromo) {
         System.out.println("selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
+        
         System.out.println("======== MENU RESTO KAFE ========");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -17,9 +18,22 @@ public class Kafe20 {
         System.out.println("6. Mie Goreng - Rp 28,000");
         System.out.println("============================");
         System.out.println("Silahkan pilih menu yang anda inginkan");
+    
+        if (KodePromo.equalsIgnoreCase("DISKON50")) {
+            System.out.println("mendapatkan diskon 50%");
+        } else if(KodePromo.equalsIgnoreCase(("DISKON30"))) {
+            System.out.println("mendapatkan diskon 30%");
+        } else  {
+            System.out.println("INVALID");
+        }
     }
 
         public static void main(String[] args){
-            menu("Andi", true);
+            menu("Andi", true, "DISKON50");
+            System.out.println();
+            menu("Andi", true, "DISKON30");
+            System.out.println();
+            menu("Andi", true, "");
         }
+
 }
