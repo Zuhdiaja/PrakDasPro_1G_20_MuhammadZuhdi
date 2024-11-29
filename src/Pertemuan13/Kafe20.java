2938b81bc82a0355919d322409c81e9e52b2ef91
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2024-11-29 13:53:00 +0700

    Pertemuan13_percobaan2

--- src/Pertemuan13/Kafe20.java
+++ src/Pertemuan13/Kafe20.java
@@ -2,7 +2,12 @@ package Pertemuan13;
 
 public class Kafe20 {
 
-    public static void main(String[] args) {
+    public static void menu(String namaPelanggan, boolean isMember) {
+        System.out.println("selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
         System.out.println("======== MENU RESTO KAFE ========");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -13,4 +18,8 @@ public class Kafe20 {
         System.out.println("============================");
         System.out.println("Silahkan pilih menu yang anda inginkan");
     }
+
+        public static void main(String[] args){
+            menu("Andi", true);
+        }
 }
