d114865c8ae848fe8dcdbf9a361c34cef0ce2ab8
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2024-11-29 21:14:22 +0700

    pertemuan13_percobaan4

---
+++ src/Pertemuan13/PengunjungKafe20.java
@@ -0,0 +1,15 @@
+package Pertemuan13;
+
+public class PengunjungKafe20 {
+    
+    public static void daftarPengunjung(String... namaPengunjung){
+        System.out.println("Daftar nama pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+    }
+}
